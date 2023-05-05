a285974a972509b87ff883e9d5e5cea8925c8c9c
Khethelo Nkosi <saneleknkosi@gmail.com>
2023-05-05 13:44:31 +0200

    Added functionality to broadcast message accross Clients

--- src/main/java/za/co/ChatApp/ChatServer.java
+++ src/main/java/za/co/ChatApp/ChatServer.java
@@ -4,34 +4,54 @@ import java.net.*;
 
 public class ChatServer {
 
-    private ServerSocket ss;
+    private ServerSocket serverSocket;
 
     public ChatServer(ServerSocket ss) {
-        this.ss = ss;
+        this.serverSocket = ss;
     }
 
     public void startServer() {
-        try {
-            while (!ss.isClosed()) {
-                Socket socket = ss.accept();
-                System.out.println("A new client has connected!");
-                ClientHandler clientHandler = new ClientHandler(socket);
+        new Thread( new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Awaiting connections...\n");
+                    while (!serverSocket.isClosed()) {
+                        Socket socket = serverSocket.accept();
+                        ClientHandler clientHandler = new ClientHandler(socket);
 
-                Thread thread = new Thread(clientHandler);
-                thread.start();
+                        System.out.println(clientHandler.getUserName()+ " has joined the chat");
+                        broadcastMessage(clientHandler.getUserName()+ " has joined the chat");
+                        
+                        Thread thread = new Thread(clientHandler);
+                        thread.start();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Cannot start server!");
+                }
             }
-        } catch (IOException e) {
-            // TODO: handle exception
-        }
+        }).start();
+        
     }
 
     public void closeServerSocket() {
         try {
-            if (ss != null) {
-                ss.close();
+            if (serverSocket != null) {
+                serverSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler c : ClientHandler.clientHandlers) {
+            try {
+                    c.out.println(message);
+                    c.out.flush();
+            } catch (Exception e) {
+                System.out.println("Could not send to: "+c.getUserName());
+            }
         }
     }
     public static void main(String[] args) throws IOException {
